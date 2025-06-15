3a68295b26c3b1ebb1b7f0644ca3f7a6cb04324c
Andrej <antandrej2003@gmail.com>
2025-06-15 20:18:04 +0200

    Adding Prototype textures and Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,40 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float forwardSpeed = 5f; 
+    public float strafeSpeed = 4f;
+
+    private CharacterController controller;
+
+    void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
+        if (controller == null)
+        {
+            Debug.LogError("CharacterController not found.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 forwardMovement = transform.forward * forwardSpeed * Time.deltaTime;
+
+        float horizontalInput = 0f;
+
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontalInput = -1f;
+        }
+        else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontalInput = 1f;
+        }
+
+        Vector3 strafeMovement = transform.right * horizontalInput * strafeSpeed * Time.deltaTime;
+        Vector3 totalMovement = forwardMovement + strafeMovement;
+
+        controller.Move(totalMovement);
     }
 }
